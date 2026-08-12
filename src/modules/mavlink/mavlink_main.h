38ed93f78b496f1f77b83baad173349d68d49cb1
ncerzzk <huangcmzzk@gmail.com>
2026-08-12 23:10:41 +0800

    mavlink: support broadcasting on all interfaces

--- src/modules/mavlink/mavlink_main.h
+++ src/modules/mavlink/mavlink_main.h
@@ -625,10 +625,11 @@ private:
 
 	sockaddr_in		_myaddr {};
 	sockaddr_in		_src_addr {};
-	sockaddr_in		_bcast_addr {};
+	static constexpr unsigned MAX_BROADCAST_ADDRESSES = 4;
+	sockaddr_in		_bcast_addrs[MAX_BROADCAST_ADDRESSES] {};
 
 	bool			_src_addr_initialized{false};
-	bool			_broadcast_address_found{false};
+	unsigned		_broadcast_address_count{0};
 	bool			_broadcast_address_not_found_warned{false};
 	bool			_broadcast_failed_warned{false};
 
